2128a9e08a4925075e600ccce2ed937d1e69dec9
Jari Knoop <144428260+JariKN01@users.noreply.github.com>
2024-08-28 15:19:46 +0200

    Adding Project component and cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,9 +5,14 @@ function Project() {
     return (
         <section id="project">
             <div className="project-container">
-                <div className="text">
-                    <h1>Project</h1>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus autem dicta dolorem eligendi officia omnis, optio, quae quidem quis sit tenetur voluptates. Esse nulla quis voluptatem? Fugiat, in, praesentium. </p>
+                <h1 className="h1-project">Projects</h1>
+                <div className="list">
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
+                    <div className="item"></div>
                 </div>
             </div>
         </section>
